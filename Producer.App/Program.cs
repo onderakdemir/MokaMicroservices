7f76834c7e392aaba9f0bdff5e69f75d3903768a
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2024-02-12 16:45:13 +0300

    Add project files.

---
+++ Producer.App/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+
+Console.WriteLine("Producer");
+
+var connectionFactory = new ConnectionFactory
+{
+    Uri = new Uri("amqps://lukszugl:fY5INvxK2sDQVPJlZvW23nXZfjDKK4lI@fish.rmq.cloudamqp.com/lukszugl")
+};
+
+using var connection = connectionFactory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+channel.QueueDeclare("hello-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+
+var message = "Merhaba RabbitMQ";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+Enumerable.Range(1, 1000).ToList().ForEach(x =>
+{
+    channel.BasicPublish(exchange: "", routingKey: "hello-queue", basicProperties: null, body: body);
+});
+
+
+Console.WriteLine("Mesajlar gönderildi.");
